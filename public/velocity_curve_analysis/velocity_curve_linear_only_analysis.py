19d56749b483ed27ba1fcb3702ad5059c301e840
jasminelong <jasmineeven2@gmail.com>
2025-10-17 17:47:58 +0900

    w

--- public/velocity_curve_analysis/velocity_curve_linear_only_analysis.py
+++ public/velocity_curve_analysis/velocity_curve_linear_only_analysis.py
@@ -228,7 +228,7 @@ def main():
         axes[i].set_ylim(y_min, y_max)
     
     plt.tight_layout()
-    plt.savefig('velocity_curves_linear_only_mean_background_opaque.png', dpi=300, bbox_inches='tight', transparent=False)
+    plt.savefig('velocity_curves_linear_only_mean_background_opaque.png', dpi=300, bbox_inches='tight', facecolor='white', transparent=False)
     try:
         plt.show()
     except Exception:
